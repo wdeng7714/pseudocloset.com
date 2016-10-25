00cb02ebb58b93d03801d6c01ae0c8781f243027
changangela <angelachang27@gmail.com>
2016-10-25 16:39:40 -0400

    Updated register.php to incorporate sign up form and mySQL updateing

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	include_once "connectdb.php";
+	header("Location: register.php");
+?>
+
+<html>
+	<head>
+		<title>PseudoCloset</title>
+	</head>
+	<body>
+		<h1>
+			Success!
+		</h1>
+	</body>
+</html>
