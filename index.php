f4df2f7bc1a0a1ed7543e371f009dcd38599bb06
wendydeng <wendy9852@hotmail.com>
2016-10-30 22:15:08 -0400

    added nav bar

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <?php
-	include_once "connectdb.php";
-	header("Location: register.php");
+include_once "connectdb.php";
+header("Location: register.php");
 ?>
 
 <html>
-	<head>
-		<title>PseudoCloset</title>
-	</head>
-	<body>
+<head>
+	<title>PseudoCloset</title>
+	<link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type="text/css"/>
+</head>
+<body>
+	<nav class = "navbar navbar-default" role = "navigation">
+		<div class = "container-fluid">
+			<div class= " navbar-header">
+				<button type = "button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#navbar">
+					<span class ="sr-only"> Toggle navigation </span>
+					<span class ="icon-bar"></span>
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+				</button>
+				<a class = "navbar-brand" href= "index.php">
+					PseudoCloset </a>
+				</div>
+
+				<div class = "collapse navbar-collapse" id = "navbar">
+					<ul class = "nav navbar-nav navbar-right">
+						<li><a href = "login.php">Login</a></li>
+						<li><a href = "register.php">Sign Up</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<script src = "vendor/jquery/jquery-3.1.0.min.js"></script>
+		<script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
 	</body>
-</html>
+	</html>
