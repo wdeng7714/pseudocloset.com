926e6f463ce80aaf9b17595436d2007711bb0e95
changangela <angelachang27@gmail.com>
2016-10-30 22:41:57 -0400

    Added session recognition to index.php

--- index.php
+++ index.php
@@ -1,31 +1,54 @@
 <?php
-include_once "connectdb.php";
-header("Location: register.php");
+	session_start();
+	include_once "connectdb.php";
 ?>
 
+<!DOCTYPE html>
+
 <html>
-<head>
-	<title>PseudoCloset</title>
-	<link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type="text/css"/>
-</head>
-<body>
-	<nav class = "navbar navbar-default" role = "navigation">
-		<div class = "container-fluid">
-			<div class= " navbar-header">
-				<button type = "button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#navbar">
-					<span class ="sr-only"> Toggle navigation </span>
-					<span class ="icon-bar"></span>
-					<span class = "icon-bar"></span>
-					<span class = "icon-bar"></span>
-				</button>
-				<a class = "navbar-brand" href= "index.php">
-					PseudoCloset </a>
+	<head>
+		<title>Home | PseudoCloset</title>
+		<meta content = "width = device.width , initial-scale = 1.0" name = "viewport">
+		<link rel = "stylesheet" href = "css/main.css" type = "text/css"/>
+		<link rel = "stylesheet" href = "vendor/font-awesome/css/font-awesome.min.css"/>
+		<link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/> 
+	</head>
+	<body>
+		<nav class = "navbar navbar-default" role = "navigation">
+			<div class = "container-fluid">
+				<div class= " navbar-header">
+					<button type = "button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#navbar">
+						<span class ="sr-only"> Toggle navigation </span>
+						<span class ="icon-bar"></span>
+						<span class = "icon-bar"></span>
+						<span class = "icon-bar"></span>
+					</button>
+					<a class = "navbar-brand" href= "index.php">
+						PseudoCloset
+					</a>
 				</div>
+
 
 				<div class = "collapse navbar-collapse" id = "navbar">
 					<ul class = "nav navbar-nav navbar-right">
-						<li><a href = "login.php">Login</a></li>
-						<li><a href = "register.php">Sign Up</a></li>
+						<?php if(isset($_SESSION['userid'])){?>
+							<li>
+								<p class = "navbar-text">
+									Signed in as <?php echo $_SESSION['username'];?>
+								</p>
+							</li>
+							<li>
+								<a href = "logout.php">Logout</a>
+							</li>
+						<?php } else { ?>
+							<li>
+								<a href = "login.php">Login</a>
+							</li>
+							<li>
+								<a href = "register.php">Sign Up</a>
+							</li>
+						<?php } ?>
 					</ul>
 				</div>
 			</div>
@@ -33,4 +56,4 @@ header("Location: register.php");
 		<script src = "vendor/jquery/jquery-3.1.0.min.js"></script>
 		<script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
 	</body>
-	</html>
+</html>
