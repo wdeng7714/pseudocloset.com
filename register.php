4ff90b1ea6d52b7d18867824d61e19ffddd0c1e9
changangela <angelachang27@gmail.com>
2016-11-01 19:48:16 -0400

    updated font for all pages

--- register.php
+++ register.php
@@ -67,6 +67,8 @@
 		<title>Register | PseudoCloset</title>
 		<meta content = "width = device-width, initial-scale = 1.0" name = "viewport">
 		<link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type = "text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/> 
+        <link rel = "stylesheet" href = "css/main.css" type = "text/css"/>
 	</head>
 	<body>
 		<nav class = "navbar navbar-default" role = "navigation">
