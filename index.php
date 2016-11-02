f929cf5095eb18113da489f9ed7c19057c5763d9
changangela <angelachang27@gmail.com>
2016-11-01 22:06:52 -0400

    added content to navigation bar

--- index.php
+++ index.php
@@ -33,14 +33,34 @@
 					<ul class = "nav navbar-nav navbar-right">
 						<?php if(isset($_SESSION['userid'])){?>
 							<li>
-								<p class = "navbar-text">
-									<i class="fa fa-user" aria-hidden="true"></i>
-                                	 <?php echo $_SESSION['username'];?>
-								</p>
+								<a href = "viewcloset.php">View closet</a>
 							</li>
 							<li>
-								<a href = "logout.php">Logout</a>
+								<a href = "planner.php">Planner</a>
 							</li>
+							<li class = "active">
+								<a href = "addclothing.php">
+									Add clothing
+								</a>
+							</li>
+							<li>
+								<a href = "laundrybasket.php">Laundry basket</a>
+							</li>
+                            <li>
+                            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            		<i class="fa fa-user" aria-hidden="true"></i>
+                             		 <?php echo $_SESSION['username'];?>
+                             		<span class="caret"></span>
+                             	</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="logout.php">
+											<span class = "glyphicon glyphicon-log-out"></span>
+											 Logout
+										</a>
+									</li>
+								</ul>
+                            </li>
 						<?php } else { ?>
 							<li>
 								<a href = "register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
