a81f4dda29e2b339e614fb7cd7e27a7be39bb1c2
changangela <angelachang27@gmail.com>
2016-11-01 21:41:06 -0400

    index now looks official

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 	session_start();
 	include_once "connectdb.php";
-	if (!isset($_SESSION['userid'])){
-		header("Location: register.php");
-	}
 ?>
 
 <!DOCTYPE html>
@@ -46,10 +43,10 @@
 							</li>
 						<?php } else { ?>
 							<li>
-								<a href = "login.php">Login</a>
+								<a href = "register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
 							</li>
 							<li>
-								<a href = "register.php">Sign Up</a>
+								<a href = "login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
 							</li>
 						<?php } ?>
 					</ul>
@@ -57,41 +54,63 @@
 			</div>
 		</nav>
 		
-		<div class = "container">
-			<div class = "row">
-				<div class = "col-md-12">
-					<h2 class = "text-center">
-						Welcome to <?php echo $_SESSION['username'];?>'s PseudoCloset
-					</h2>
+		<?php
+			if (isset($_SESSION['userid'])){ ?>
+
+			<div class = "container">
+				<div class = "row">
+					<div class = "col-md-12">
+						<h2 class = "text-center">
+							Welcome to <?php echo $_SESSION['username'];?>'s PseudoCloset
+						</h2>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "col-md-8 well">
+						<h3 class = "text-center"> Alerts </h3>
+						<?php 
+							$query = "SELECT * FROM clothing WHERE userid = " . $_SESSION["userid"];
+							$result = mysqli_query($con, $query);
+							while($row = mysqli_fetch_array($result)){
+								if ($row['timesworn']>=1){
+									echo "<div> <p>". $row["name"]. " has been worn ". $row["timesworn"] . " times, it's time for a wash! " . $row["name"] ." has been placed in your laundry basket. </p> </div>";	
+								}
+							}
+						?> 
+						
+					</div>
+					<div class = "col-md-4">
+						<ul>
+							<a href = "viewcloset.php" class = "btn btn-default btn-block">View Closet</a>
+							<a href = "planner.php" class = "btn btn-default btn-block">Planner</a>
+							<a class = "btn btn-default btn-block" href = "addclothing.php">Add Clothing
+							</a>
+							<a href = "laundrybasket.php" class = "btn btn-default btn-block">Laundry Basket
+							</a>
+						</ul>
+					</div>
+				</div> 
+			</div>
+
+		<?php }else{?>
+			<div class = "jumbotron">
+				<div class = "container">
+					<h1>Welcome to PseudoCloset</h1>
+					<p>
+						You ever had that moment when you stare blankly into your cloest and can't figure out what to wear? Well worry no more because you have found just the right place for all your problems...
+					</p>
+					<button class = "btn btn-primary">
+						Learn more &raquo
+					</button>
 				</div>
 			</div>
-			<div class = "row">
-				<div class = "col-md-8 well">
-					<h3 class = "text-center"> Alerts </h3>
-					<?php 
-						$query = "SELECT * FROM clothing WHERE userid = " . $_SESSION["userid"];
-						$result = mysqli_query($con, $query);
-						while($row = mysqli_fetch_array($result)){
-							if ($row['timesworn']>=1){
-								//echo "<div> <p>". "</p> </div>";
-								echo "<div> <p>". $row["name"]. " has been worn ". $row["timesworn"] . " times, it's time for a wash! " . $row["name"] ." has been placed in your laundry basket. </p> </div>";	
-							}
-						}
-					?> 
-					
+
+			<footer class="footer">
+				<div class="container">
+					<p class="text-muted text-center">&copy; Copyright Angela Chang and Wendy Deng 2016, All Rights Reserved</p>
 				</div>
-				<div class = "col-md-4">
-					<ul>
-						<a href = "viewcloset.php" class = "btn btn-default btn-block">View Closet</a>
-						<a href = "planner.php" class = "btn btn-default btn-block">Planner</a>
-						<a class = "btn btn-default btn-block" href = "addclothing.php">Add Clothing
-						</a>
-						<a href = "laundrybasket.php" class = "btn btn-default btn-block">Laundry Basket
-						</a>
-					</ul>
-				</div>
-			</div> 
-		</div>
+			</footer>
+		<?php }?>
 		<script src = "vendor/jquery/jquery-3.1.0.min.js"></script>
 		<script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
 	</body> 
