0a43d0611a09416c20da108f01729f4f7e1da105
changangela <angelachang27@gmail.com>
2016-11-04 19:20:50 -0400

    created working modals with thumbnail gallery

--- js/main.js
+++ js/main.js
@@ -6,14 +6,46 @@ $( document ).ready(function() {
     	$(this).addClass('active');
 
     	var viewValue = this.getAttribute('id');
-    	if(viewValue != 'new'){
-			$.post( 
-	        	"viewcloset.php",{view: viewValue},
-	            function(data) {
-	            	$('#viewstage').empty().html(data);
-	            }
-	        );
-		}
+    	if(viewValue == "all"){
+    		$('.thumbnail-item').removeClass("thumbnail-hide");
+    	}
+    	else if(viewValue == "tops"){
+    		$('.thumbnail-item').addClass("thumbnail-hide");
+    		$('[type = "sweater"').parents().removeClass("thumbnail-hide");
+    		$('[type = "shirt"').parents().removeClass("thumbnail-hide");
+    	}else if(viewValue == "bottoms"){
+			$('.thumbnail-item').addClass("thumbnail-hide");
+			$('[type = "pants"').parents().removeClass("thumbnail-hide");
+    	}else if(viewValue == "misc"){
+    		$('.thumbnail-item').addClass("thumbnail-hide");
+    		$('[type = "socks"').parents().removeClass("thumbnail-hide");
+    	}else if(viewValue == "outfits"){
+    		$('.thumbnail-item').addClass("thumbnail-hide");
+    	}
+
+  //   	if(viewValue != 'new'){
+		// 	$.post( 
+	 //        	"viewcloset.php",{view: viewValue},
+	 //            function(data) {
+	 //            	$('#viewstage').empty().html(data);
+	 //            }
+	 //        );
+		// }
     });
+
+    $('.thumbnail').click(function(){
+    	// $('.modal-body').empty();
+    	var name = $(this).attr('name');
+    	var url = $(this).attr('url');
+    	var color = $(this).attr('color');
+    	var timesworn = $(this).attr('timesworn');
+    	var lastworn = $(this).attr('lastworn'); 
+    	var type = $(this).attr('type');
+
+    	$('.modal-title').text(name);
+
+    	$('.modal-body .img-responsive').attr('src',url);
+    });
+
 
 });
--- viewcloset.php
+++ viewcloset.php
@@ -12,40 +12,6 @@
 	$max_per_page = 20;
 	$result_copy = $result;
 
-	if(isset($_POST['view'])){
-		echo '<h4>View: ' . $_POST['view'] . '</h4>';
-		if(($_POST['view'] == 'all')){
-			while ($row = mysqli_fetch_array($result_copy) and $display<$max_per_page){
-				$display++;
-				echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6"><a href = "#" class = "thumbnail"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
-			}
-		}
-		elseif($_POST['view'] == 'tops'){
-			while ($row = mysqli_fetch_array($result_copy) and $display<$max_per_page){
-				$display++;
-				if($row['type'] == 'sweater' || $row['type'] == 'shirt'){
-					echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6"><a href = "#" class = "thumbnail"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
-				}
-			}
-		}
-		elseif($_POST['view'] == 'bottoms'){
-			while ($row = mysqli_fetch_array($result_copy) and $display<$max_per_page){
-				$display++;
-				if($row['type'] == 'pants'){
-					echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6"><a href = "#" class = "thumbnail"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
-				}
-			}
-		}
-		elseif($_POST['view'] == 'misc'){
-			while ($row = mysqli_fetch_array($result_copy) and $display<$max_per_page){
-				$display++;
-				if($row['type'] == 'socks'){
-					echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6"><a href = "#" class = "thumbnail"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
-				}
-			}
-		}
-	}
-	else{
 ?>
 <!DOCTYPE html>
 <html>
@@ -137,16 +103,34 @@
 					<?php
 						while ($row = mysqli_fetch_array($result_copy) and $display<$max_per_page){
 							$display++;
-							echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6"><a href = "#" class = "thumbnail"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
+							echo '<div class = "col-lg-3 col-md-4 col-sm-6 col-xs-6 thumbnail-item"><a href = "#itemmodal" data-toggle = "modal" class = "thumbnail" color ="' . $row['color'] .'" timesworn="' . $row['timesworn'] . '" name = "' . $row['name'] . '" url = "' . $row['url'] .'" lastworn = "' . $row['lastworn'] . '" type = "'.$row['type'].'"><p>' . $row['name'] . '</p><img src="' . $row["url"] . '"></a></div>';
 						}
 					?>
 				</div>
 			</div>
 		</div>
+
+			<div class="modal fade" id="itemmodal" role="dialog">
+				<div class = "modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title"></h4>
+						</div>
+						<div class="modal-body">
+							<img src = "" class = "img-responsive center-block"></img>
+							<p></p>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<script src = "vendor/jquery/jquery-3.1.0.min.js"></script>
         <script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
         <script src = "js/main.js"></script>
 	</body>
 </html>
-
-<?php } ?>
