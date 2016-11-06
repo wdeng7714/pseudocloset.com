77ab4c09edf02b13a8803e43e381f4715cfd4627
wendydeng <wendy9852@hotmail.com>
2016-11-06 15:05:21 -0500

    made changes to laundrybasket.php

---
+++ laundrybasket.php
@@ -0,0 +1,140 @@
+<?php
+	session_start();
+	include_once "connectdb.php";
+	if (!isset($_SESSION['userid'])){
+		header("Location: index.php");
+	}
+
+	$query = "SELECT * FROM clothing WHERE userid = " . $_SESSION["userid"] . " AND timesworn > 2";
+	$result = mysqli_query($con, $query);
+
+	$item_display=0;
+	$result_copy = $result;
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Laundry Basket | PseudoCloset </title>
+		<meta content = "width = device.width , initial-scale = 1.0" name = "viewport">
+		<link rel = "stylesheet" href = "vendor/font-awesome/css/font-awesome.min.css"/>
+		<link rel = "stylesheet" href = "vendor/bootstrap/css/bootstrap.min.css" type="text/css"/>
+		<link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.carousel.css" type="text/css"/>
+		<link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.theme.css" type="text/css"/>
+		<link rel = "stylesheet" href = "vendor/owl-carousel/css/owl.transitions.css" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet"/> 
+		<link rel = "stylesheet" href = "css/main.css" type = "text/css"/>
+	</head>
+	<body>
+		<nav class = "navbar navbar-default" role = "navigation">
+            <div class = "container-fluid">
+                <div class= " navbar-header">
+                    <button type = "button" class = "navbar-toggle" data-toggle ="collapse" data-target = "#navbar">
+                        <span class ="sr-only"> Toggle navigation </span>
+                        <span class ="icon-bar"></span>
+                        <span class = "icon-bar"></span>
+                        <span class = "icon-bar"></span>
+                    </button>
+                    <a class = "navbar-brand" href= "index.php">
+                        PseudoCloset
+                    </a>
+                </div>
+
+                <div class = "collapse navbar-collapse" id = "navbar">
+                    <ul class = "nav navbar-nav navbar-right">
+                        <?php if(isset($_SESSION['userid'])){?>
+							<li>
+								<a href = "viewcloset.php">View closet</a>
+							</li>
+							<li>
+								<a href = "planner.php">Planner</a>
+							</li>
+							<li class = "active">
+								<a href = "laundrybasket.php">Laundry basket</a>
+							</li>
+							<li>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                             		 <i class = "icon-plus"></i> Add
+                             		<span class="caret"></span>
+                             	</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="addclothing.php">
+											 Add clothing
+										</a>
+										<a href="addoutfit.php">
+											 Add outfit
+										</a>
+										<a href="addplan.php">
+											 Add plan
+										</a>
+									</li>
+								</ul>
+							</li>
+                            <li>
+                            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            		<i class="icon-user" aria-hidden="true"></i>
+                             		 <?php echo $_SESSION['username'];?>
+                             		<span class="caret"></span>
+                             	</a>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="logout.php">
+											<span class = "glyphicon glyphicon-log-out"></span>
+											 Logout
+										</a>
+									</li>
+								</ul>
+                            </li>
+                        <?php } else { ?>
+							<li>
+								<a href = "register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+							</li>
+							<li>
+								<a href = "login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+							</li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class = "container"> 
+			<div class ="row" >
+				<div class = "col-lg-12">
+					<h2 class = "text-center page-header">Laundry Basket</h2>
+				</div>
+				<div class = "col-md-12">
+
+					<!-- Centered Pills -->
+					<ul class="nav nav-pills nav-justified" id = "views" >
+						<li id = "all"><a href ="#" class= "" >All</a></li>
+ 						<li id = "tops"><a href ="#" class= "" >Tops</a></li>
+						<li id = "bottoms"><a href ="#" class= "" >Bottoms</a></li>
+						<li id = "misc"><a href ="#" class= "" >Misc</a></li>
+					</ul>
+				</div>
+
+				<div class = "col-lg-12" id = "viewstage">
+					<h4>View: all</h4>
+					<?php
+						while ($row = mysqli_fetch_array($result)) {
+							echo '<div class ="col-lg-3 col-md-4 col-sm-6 col-xs-6"><div class = "item planner-item"><a class = "thumbnail" color ="' . $row['color'] .'" timesworn="' . $row['timesworn'] . '" name = "' . $row['name'] . '" url = "' . $row['url'] .'" lastworn = "' . $row['lastworn'] . '" type = "'.$row['type'].'" id = "'. $row['id'] .'"><p>' . $row['name'] . '<span class = "pull-right"><i class="icon-check-empty" id = "checkbox' .$row['id'] . '"></i></span></p><img src="' . $row["url"] . '"></a></div></div>';
+						}
+						
+					?>
+
+					<div class = "thumbnail-hide" id = "outfits-gallery">
+						<?php include "outfits-gallery.php"; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<script src = "vendor/jquery/jquery-2.1.1.min.js"></script>
+        <script src = "vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src = "vendor/owl-carousel/js/owl.carousel.min.js"></script>
+        <script src = "js/main.js"></script>
+	</body>
+</html>
